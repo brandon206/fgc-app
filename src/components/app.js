efbf841f44c7f9e730682fdff31bb2156f6d6212
Brandon Park <brandonpark206@gmail.com>
2019-01-25 17:40:41 -0800

    testing routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,37 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
-import React from 'react';
+import React , {Component, Fragment} from 'react';
 import PlayerProfile from './player-profile';
 import Leaderboard from './leaderboard';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
-const App = () => (
-    <div>
-        <Route exact path = "/" component = {Leaderboard}/>
-        <Route path = "/player-profile" component = {PlayerProfile}/>
-    </div>
-);
+class App extends Component {
+    render () {
+        return(
+            <div className = "container">
+
+                <Fragment>
+                    <ul>
+                        <li>
+                        <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                        <Link to="/player-profile">Users</Link>
+                        </li>
+                    </ul>
+                </Fragment>
+
+                <Route exact path = '/' render = {() => {
+                    return <Leaderboard />
+                }}/>
+
+                <Route path ='/player-profile' render = {() => {
+                    return <PlayerProfile />
+                }}/>
+
+            </div>
+        );
+    }
+}
 
 export default App;
