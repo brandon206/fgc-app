68eee2f00ed33e8dd5fcff338021ee11ed17c5ba
Brandon Park <brandonpark206@gmail.com>
2019-02-06 11:38:50 -0800

    reformatted date from utc timestamp to date

--- src/components/player-profile/player-profile.js
+++ src/components/player-profile/player-profile.js
@@ -9,6 +9,8 @@ import playerprofilecss from './player-profile.css';
 class PlayerProfile extends Component {
 
     componentDidMount() {
+        //after render brings to top of the page
+        window.scrollTo(0, 0);
         this.props.getPlayerData(this.props.location.pathname.match(/(\d+)/)[0]);
     }
 
@@ -21,7 +23,7 @@ class PlayerProfile extends Component {
             );
         }
         const { player_data } = this.props;
-        console.log("props: ", this.props);
+        console.log("date: ", new Date(this.props.player_data.player.player_data.results[0].date));
         console.log("location path: ", this.props.location.pathname.match(/(\d+)/)[0]);
         let sortedArr = this.props.player_data.player.player_data.results;
         sortedArr.sort(function compare(a, b) {
@@ -52,10 +54,9 @@ class PlayerProfile extends Component {
                             <ul key={tournament.tournamentname}>
                                 <li className = "tournament-name">{`Tournament: ${tournament.tournamentname}`}</li>
                                 <li className = "tournament-game">{`Game: ${tournament.game}`}</li>
-                                <li className = "tournament-date">{`Date: ${tournament.date}`}</li>
+                                <li className = "tournament-date">{`Date: ${new Date(tournament.date).toDateString()}`}</li>
                                 <li className = "tournament-type">{`Type: ${tournament.type}`}</li>
                                 <li className = "tournament-place">{`Place: ${tournament.place}`}</li>
-
                             </ul>
                         ))}
                         </Fragment>
