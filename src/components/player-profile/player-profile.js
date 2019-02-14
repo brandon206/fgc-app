f322e2c7e2766ac6dfeb6d9b96e79988bee82400
Brandon Park <brandonpark206@gmail.com>
2019-02-14 13:33:29 -0800

    fixing profile page layout

--- src/components/player-profile/player-profile.js
+++ src/components/player-profile/player-profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPlayerData } from '../../actions/';
 import playerprofilecss from './player-profile.css';
+import gokuImg from '../../assets/images/goku_black.jpg';
 
 
 class PlayerProfile extends Component {
@@ -11,14 +12,28 @@ class PlayerProfile extends Component {
     componentDidMount() {
         //after render brings to top of the page
         window.scrollTo(0, 0);
-        this.props.getPlayerData(this.props.location.pathname.match(/(\d+)/)[0]);
+        const playerID = this.props.location.pathname.match(/(\d+)/)[0];
+        this.props.getPlayerData(playerID);
     }
 
     render (){
-        if(this.props.player_data.player.player_data.length === 0){
+        const playerID = this.props.location.pathname.match(/(\d+)/)[0];
+
+        if(this.props.player_data.player.player_data.length === 0 || 
+            this.props.player_data.player.player_data.id != playerID){
             return (
-                <div className="container progress">
-                    <div className="indeterminate"></div>
+                <div className="spinner-container">
+                    <div className="preloader-wrapper big active">
+                        <div className="spinner-layer spinner-blue-only">
+                        <div className="circle-clipper left">
+                            <div className="circle"></div>
+                        </div><div className="gap-patch">
+                            <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                            <div className="circle"></div>
+                        </div>
+                        </div>
+                    </div>
                 </div>
             );
         }
@@ -39,27 +54,44 @@ class PlayerProfile extends Component {
                     <div>
                         <img className = "dbfz-banner" src= {dbfz} alt="dbfz wallpaper"/>
                     </div>
-                    <h1 className="dbfz-header center">{`${data.name} Profile`}</h1>
-                        <div className = "player-info">{`Teams: ${data.teams.map((item) => {
-                                return (item);
-                            })}`}</div>
-                        <div className="player-info">{`Main Game: ${data.mainGame}`}</div>
+                    <div className="row">
+                        <div className="col s12">
+                            <div className="card orange lighten-3">
+                                <div className="card-content">
+                                <h1 className="dbfz-header center">{`${data.name} Profile`}</h1>
+                                <div className="img-container">
+                                    <img className = "player-image" src = {gokuImg}></img>
+                                </div>
+                                <div className = "player-info">{`Teams: ${data.teams.map((item) => {
+                                        return (item);
+                                    })}`}</div>
+                                <div className="player-info">{`Main Game: ${data.mainGame}`}</div>
 
-                        <div className = "player-info">{`Alltime Ranking: ${data.rankings.DBFZ.alltimeRank}`}</div> 
-                        <div className="player-info">{`Main Characters: ${data.rankings.DBFZ.main.map((character) => {
-                            return (` ${character.substring(5)}`);
-                        })}`}</div>
-                        <Fragment>
-                        {sortedArr.map(tournament => (
-                            <ul key={tournament.tournamentname}>
-                                <li className = "tournament-name">{`Tournament: ${tournament.tournamentname}`}</li>
-                                <li className = "tournament-game">{`Game: ${tournament.game}`}</li>
-                                <li className = "tournament-date">{`Date: ${new Date(tournament.date).toDateString()}`}</li>
-                                <li className = "tournament-type">{`Type: ${tournament.type}`}</li>
-                                <li className = "tournament-place">{`Place: ${tournament.place}`}</li>
-                            </ul>
-                        ))}
-                        </Fragment>
+                                <div className = "player-info">{`Alltime Ranking: ${data.rankings.DBFZ.alltimeRank}`}</div> 
+                                <div className="player-info">{`Main Characters: ${data.rankings.DBFZ.main.map((character) => {
+                                    return (` ${character.substring(5)}`);
+                                })}`}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12">
+                            <Fragment>
+                            {sortedArr.map(tournament => (
+                                <ul key={tournament.tournamentname}>
+                                    <div class="card-panel grey lighten-3">
+                                        <li className = "tournament-name">{`Tournament: ${tournament.tournamentname}`}</li>
+                                        <li className = "tournament-game">{`Game: ${tournament.game}`}</li>
+                                        <li className = "tournament-date">{`Date: ${new Date(tournament.date).toDateString()}`}</li>
+                                        <li className = "tournament-type">{`Type: ${tournament.type}`}</li>
+                                        <li className = "tournament-place">{`Place: ${tournament.place}`}</li>
+                                    </div>
+                                </ul>
+                            ))}
+                            </Fragment>
+                        </div>
+                    </div>
                 </div>
             );
     }
